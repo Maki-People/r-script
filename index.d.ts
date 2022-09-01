e49dc9038b3f4511839ae0270939549983eacb52
Jérôme Duval <jerome.duval@hikipeople.com>
2022-09-01 11:52:30 +0200

    add type declaration file

--- index.d.ts
+++ index.d.ts
@@ -8,12 +8,17 @@ export declare class R {
     options: {
         env: {
             input: string;
-            TZ?: string | undefined;
+            TZ?: string;
             DIRNAME: string;
         };
         encoding: string;
     };
     constructor(path: string);
     data(...args: unknown[]): void;
+    /**
+     * This method execute the R script given in R class initiation
+     * @param timeout Time in ms before the Rscript process is killed.
+     * @returns Output from the R script.
+     */
     execute(timeout?: number): Promise<string>;
 }
--- src/index.js
+++ src/index.js
@@ -1,83 +1,123 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
 };
-Object.defineProperty(exports, "__esModule", { value: true });
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+exports.__esModule = true;
 exports.R = void 0;
-const child_process = __importStar(require("child_process"));
-class R {
-    constructor(path) {
+var child_process = require("child_process");
+var R = /** @class */ (function () {
+    function R(path) {
         this.arguments = {};
         this.command = "Rscript";
         this.commandArgs = ["--vanilla", __dirname + "/R/launch.R"];
         this.options = {
-            env: { DIRNAME: __dirname, ...process.env, input: "" },
-            encoding: "utf8",
+            env: __assign(__assign({ DIRNAME: __dirname }, process.env), { input: "" }),
+            encoding: "utf8"
         };
         this.path = path;
     }
-    data(...args) {
-        for (let i = 0; i < args.length; i++) {
+    R.prototype.data = function () {
+        var args = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            args[_i] = arguments[_i];
+        }
+        for (var i = 0; i < args.length; i++) {
             this.arguments[i] = args[i];
         }
-    }
-    async execute(timeout) {
-        this.options.env["input"] = JSON.stringify([this.arguments, this.path, {}]);
-        const spawnPromise = (cmd, args) => {
-            return new Promise((resolve, reject) => {
-                try {
-                    const childProcess = child_process.spawn(cmd, args, this.options);
-                    let timeoutReference;
-                    if (timeout) {
-                        timeoutReference = setTimeout(() => {
-                            childProcess.kill();
-                            try {
-                                throw new Error("Rscript process timeout");
+    };
+    /**
+     * This method execute the R script given in R class initiation
+     * @param timeout Time in ms before the Rscript process is killed.
+     * @returns Output from the R script.
+     */
+    R.prototype.execute = function (timeout) {
+        return __awaiter(this, void 0, void 0, function () {
+            var spawnPromise;
+            var _this = this;
+            return __generator(this, function (_a) {
+                this.options.env["input"] = JSON.stringify([this.arguments, this.path, {}]);
+                spawnPromise = function (cmd, args) {
+                    return new Promise(function (resolve, reject) {
+                        try {
+                            var childProcess_1 = child_process.spawn(cmd, args, _this.options);
+                            var timeoutReference_1;
+                            if (timeout) {
+                                timeoutReference_1 = setTimeout(function () {
+                                    childProcess_1.kill();
+                                    try {
+                                        throw new Error("Rscript process timeout");
+                                    }
+                                    catch (error) {
+                                        reject(error);
+                                    }
+                                }, timeout);
                             }
-                            catch (error) {
-                                reject(error);
-                            }
-                        }, timeout);
-                    }
-                    let body = "";
-                    childProcess.stdout.on("data", (data) => {
-                        body += data.toString();
+                            var body_1 = "";
+                            childProcess_1.stdout.on("data", function (data) {
+                                body_1 += data.toString();
+                            });
+                            childProcess_1.stdout.once("close", function () {
+                                if (timeout)
+                                    clearTimeout(timeoutReference_1);
+                                resolve(body_1);
+                            });
+                            childProcess_1.stderr.on("data", function (data) {
+                                throw new Error(data.toString());
+                            });
+                        }
+                        catch (error) {
+                            reject(error);
+                        }
                     });
-                    childProcess.stdout.once("close", () => {
-                        if (timeout)
-                            clearTimeout(timeoutReference);
-                        resolve(body);
-                    });
-                    childProcess.stderr.on("data", (data) => {
-                        throw new Error(data.toString());
-                    });
-                }
-                catch (error) {
-                    reject(error);
-                }
+                };
+                return [2 /*return*/, spawnPromise(this.command, this.commandArgs)];
             });
-        };
-        return spawnPromise(this.command, this.commandArgs);
-    }
-}
+        });
+    };
+    return R;
+}());
 exports.R = R;
